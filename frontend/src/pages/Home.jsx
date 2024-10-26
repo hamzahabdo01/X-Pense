0c6b7d14582eb99b598451235ff3da455a6e60b4
hamzahabdo01 <hamza.abdo19@gmail.com>
2024-10-26 15:35:29 +0300

    Django plz

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import img from '../img/undraw_projections_re_ulc6.svg'
 function Home() {
     return (
-        <div className="h-[950px]">
-            <main className="container mx-auto flex flex-col md:flex-row items-center justify-between py-16 px-18 ml-[360px]">
+        <div className="h-[850px]">
+            <main className="container mx-auto flex flex-col md:flex-row items-center justify-between py-16 px-18 ml-[250px]">
                 <div className=" text-center md:text-left space-y-10">
                     <h1 className="text-8xl font-bold mb-4 tracking-tighter">Take control of <br /> your finances</h1>
                     <p className="text-md mb-6">
